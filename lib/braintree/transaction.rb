2024acbd7c716f95e85ee78e3677640decce3402
Wei Xia <weixia@paypal.com>
2023-05-25 16:28:07 +0000

    Add retry_ids and retried_transaction_id to transaction response

--- lib/braintree/transaction.rb
+++ lib/braintree/transaction.rb
@@ -152,6 +152,8 @@ module Braintree
     attr_reader :refunded_transaction_id
     attr_reader :refunded_installments
     attr_reader :retried
+    attr_reader :retry_ids                              # all retried transactions ids for a primary transaction
+    attr_reader :retried_transaction_id                 # the primary/parent transaction id of any retried transaction
     attr_reader :retrieval_reference_number
     attr_reader :risk_data
     attr_reader :samsung_pay_card_details
