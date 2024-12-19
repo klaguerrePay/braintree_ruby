8d2501e40394dfb0273ec5bcf82be679eb35935f
Kevin Laguerre <klaguerre@paypal.com>
2024-12-19 14:59:17 +0000

    added assertions for unit test

--- spec/unit/braintree/transaction/paypal_details_spec.rb
+++ spec/unit/braintree/transaction/paypal_details_spec.rb
@@ -22,7 +22,7 @@ describe Braintree::Transaction::PayPalDetails do
         :payer_status =>"status",
         :payment_id => "payment-id",
         :recipient_email => "test@paypal.com",
-        :recipient_phone => [:country_coude => "1", :national_number => "4082222222"],
+        :recipient_phone => [:country_code => "1", :national_number => "4082222222"],
         :refund_from_transaction_fee_amount => "1.00",
         :refund_from_transaction_fee_currency_iso_code => "123",
         :refund_id => "refund-id",
@@ -51,6 +51,9 @@ describe Braintree::Transaction::PayPalDetails do
       expect(details.payer_last_name).to eq("Hopper")
       expect(details.payer_status).to eq("status")
       expect(details.payment_id).to eq("payment-id")
+      expect(details.recipient_email).to eq("test@paypal.com")
+      expect(details.recipient_phone.country_code).to eq("1")
+      expect(details.recipient_phone.national_number).to eq("4082222222")
       expect(details.refund_from_transaction_fee_amount).to eq("1.00")
       expect(details.refund_from_transaction_fee_currency_iso_code).to eq("123")
       expect(details.refund_id).to eq("refund-id")
