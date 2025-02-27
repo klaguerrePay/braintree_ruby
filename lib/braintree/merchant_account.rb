4221b91ec52d2da0857049d36d153de4809705c2
Kevin Laguerre <klaguerre@paypal.com>
2025-02-27 15:06:05 +0000

    adding details for merchant account back in

--- lib/braintree/merchant_account.rb
+++ lib/braintree/merchant_account.rb
@@ -8,9 +8,13 @@ module Braintree
       Suspended = "suspended"
     end
 
+    attr_reader :business_details
     attr_reader :currency_iso_code
     attr_reader :default
+    attr_reader :funding_details
     attr_reader :id
+    attr_reader :individual_details
+    attr_reader :master_merchant_account
     attr_reader :status
 
     alias_method :default?, :default
@@ -21,6 +25,10 @@ module Braintree
 
     def initialize(gateway, attributes)
       @gateway = gateway
+      @individual_details = IndividualDetails.new(@individual)
+      @business_details = BusinessDetails.new(@business)
+      @funding_details = FundingDetails.new(@funding)
+      @master_merchant_account = MerchantAccount._new(@gateway, attributes.delete(:master_merchant_account)) if attributes[:master_merchant_account]
       set_instance_variables_from_hash(attributes)
         end
 
