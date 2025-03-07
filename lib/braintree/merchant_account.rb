a42d1ba298062500d34ba8fbbd19f2c6b0c30278
Kevin Laguerre <klaguerre@paypal.com>
2025-03-07 18:48:01 +0000

    Removes marketplace attribs

--- lib/braintree/merchant_account.rb
+++ lib/braintree/merchant_account.rb
@@ -18,13 +18,9 @@ module Braintree
       include Braintree::MerchantAccount::FundingDestination
     end
 
-    attr_reader :business_details
     attr_reader :currency_iso_code
     attr_reader :default
-    attr_reader :funding_details
     attr_reader :id
-    attr_reader :individual_details
-    attr_reader :master_merchant_account
     attr_reader :status
 
     alias_method :default?, :default
@@ -35,10 +31,6 @@ module Braintree
 
     def initialize(gateway, attributes)
       @gateway = gateway
-      @individual_details = IndividualDetails.new(@individual)
-      @business_details = BusinessDetails.new(@business)
-      @funding_details = FundingDetails.new(@funding)
-      @master_merchant_account = MerchantAccount._new(@gateway, attributes.delete(:master_merchant_account)) if attributes[:master_merchant_account]
       set_instance_variables_from_hash(attributes)
     end
 
@@ -50,7 +42,7 @@ module Braintree
     end
 
     def inspect
-      order = [:id, :status, :master_merchant_account]
+      order = [:id, :status]
       nice_attributes = order.map do |attr|
         "#{attr}: #{send(attr).inspect}"
       end
